f0542abc2f835bf1a20a23ed384ce32f1ae27972
Harivansh <hello@harivan.sh>
2022-04-26 16:40:54 +0400

    configure tailwindcss

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 // import { registerSW } from "virtual:pwa-register";
+import "./index.css";
 
 // @todo: activate when deploying
 // if ("serviceWorker" in navigator) {
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 module.exports = {
-  content: [],
+  content: ["./index.html", "./src/**/*.vue"],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
