917a9403ce755eb9627f8d33aee2bc5a66c80aaa
Harivansh <hello@harivan.sh>
2022-04-26 15:34:18 +0400

    configure vite-plugin-pwa

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,9 @@
 import { createApp } from "vue";
 import App from "./App.vue";
+import { registerSW } from "virtual:pwa-register";
+
+if (import.meta.env.PROD && "serviceWorker" in navigator) {
+  registerSW();
+}
 
 createApp(App).mount("#app");
