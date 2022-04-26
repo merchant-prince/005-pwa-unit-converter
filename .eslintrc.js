29fa30d547d4eca4c2d4dfd72347a463d38eb685
Harivansh <hello@harivan.sh>
2022-04-26 15:02:39 +0400

    add prettier config

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,7 +6,8 @@ module.exports = {
     "extends": [
         "eslint:recommended",
         "plugin:vue/vue3-recommended",
-        "plugin:@typescript-eslint/recommended"
+        "plugin:@typescript-eslint/recommended",
+        "prettier"
     ],
     "parser": "vue-eslint-parser",
     "parserOptions": {
