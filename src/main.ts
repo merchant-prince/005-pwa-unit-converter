5dbb83e724f10fe8f8b800948f10d2d55c24b8fe
Harivansh <hello@harivan.sh>
2022-04-26 16:21:43 +0400

    comment SW registration

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,10 @@
 import { createApp } from "vue";
 import App from "./App.vue";
-import { registerSW } from "virtual:pwa-register";
+// import { registerSW } from "virtual:pwa-register";
 
-if (import.meta.env.PROD && "serviceWorker" in navigator) {
-  registerSW();
-}
+// @todo: activate when deploying
+// if ("serviceWorker" in navigator) {
+//   registerSW();
+// }
 
 createApp(App).mount("#app");
