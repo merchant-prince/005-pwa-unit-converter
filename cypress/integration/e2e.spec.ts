46970ff39521f22be5a654b52c343e9d87e9b021
Harivansh <hello@harivan.sh>
2022-04-27 16:59:00 +0400

    add base e2e tests

--- cypress/integration/e2e.spec.ts
+++ cypress/integration/e2e.spec.ts
@@ -1,5 +1,71 @@
-describe("An e2e test", () => {
-  it("runs an e2e test", () => {
-    cy.visit("/").its("document").should("contain.text", "Hello Vue");
+import { units } from "../../src/lib/units";
+
+describe("Application Test", () => {
+  it("renders the application", () => {
+    cy.visit("/");
   });
+
+  it("loads empty inputs on initialization", () => {
+    cy.visit("/");
+
+    cy.get('[data-test="input"]').should("contain.value", "");
+    cy.get('[data-test="output"]').should("contain.value", "");
+  });
+
+  it("loads the correct units for each category", () => {
+    cy.visit("/");
+
+    const categories = Object.keys(units);
+
+    categories.forEach((category) => {
+      cy.get(`[data-test="category-${category}"]`)
+        .click()
+        .then(() => {
+          units[category]
+            .map((unit) => unit.name)
+            .forEach((unitName) => {
+              cy.get('[data-test="input-units"]').should(
+                "contain.text",
+                unitName
+              );
+              cy.get('[data-test="output-units"]').should(
+                "contain.text",
+                unitName
+              );
+            });
+        });
+    });
+  });
+
+  describe("length", () => {
+    describe("input --> output", () => {
+      it("converts cm to cm");
+      it("converts cm to m");
+      it("converts cm to km");
+
+      it("converts m to cm");
+      it("converts m to m");
+      it("converts m to km");
+
+      it("converts km to cm");
+      it("converts km to m");
+      it("converts km to km");
+    });
+
+    // describe("input <-- output", () => {
+    //   it("converts cm to cm")
+    //   it("converts cm to m")
+    //   it("converts cm to km")
+
+    //   it("converts m to cm")
+    //   it("converts m to m")
+    //   it("converts m to km")
+
+    //   it("converts km to cm")
+    //   it("converts km to m")
+    //   it("converts km to km")
+    // })
+  });
+
+  // tests for area, volume, temperature, weight, time
 });
