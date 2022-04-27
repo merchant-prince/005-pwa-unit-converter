384ec583cf45572ab92cfd7f059ccc2f968d58c2
Harivansh <hello@harivan.sh>
2022-04-27 09:48:40 +0400

    setup unit-selector component

---
+++ src/components/UnitSelector.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import { toRefs } from "vue";
+
+const props = defineProps<{ modelValue: string; units: string[] }>();
+const emit = defineEmits<{
+  (eventName: "update:modelValue", value: string): void;
+}>();
+
+const { units, modelValue } = toRefs(props);
+</script>
+
+<template>
+  <div
+    class="h-52 p-2 border-2 bg-white border-gray-800 rounded-md shadow-md overflow-y-auto hide-scrollbar"
+  >
+    <p
+      v-for="unit in units"
+      :key="unit"
+      class="cursor-pointer px-2 py-1 rounded-md"
+      :class="[
+        modelValue === unit
+          ? 'text-white bg-orange-400 shadow-md'
+          : 'text-gray-800',
+      ]"
+      @click="emit('update:modelValue', unit)"
+    >
+      {{ unit }}
+    </p>
+  </div>
+</template>
