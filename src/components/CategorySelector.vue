7c659868d9db09712677915fc17a03f9332982b2
Harivansh <hello@harivan.sh>
2022-04-27 09:02:29 +0400

    add category selector component

---
+++ src/components/CategorySelector.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { toRefs } from "vue";
+
+const props = defineProps<{ value: string; modelValue: string }>();
+const emit = defineEmits<{
+  (eventName: "update:modelValue", value: string): void;
+}>();
+
+const { value, modelValue } = toRefs(props);
+</script>
+
+<template>
+  <div
+    class="border-2 px-4 py-1 rounded-full"
+    :class="[
+      modelValue == value
+        ? 'text-white border-orange-400 bg-orange-400 shadow-md'
+        : 'text-black border-black',
+    ]"
+    @click="emit('update:modelValue', value)"
+  >
+    {{ value }}
+  </div>
+</template>
