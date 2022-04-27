3c648ac641d09e28c3c998fa61d8c6483a68d690
Harivansh <hello@harivan.sh>
2022-04-27 19:59:48 +0400

    activate SW registration

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,10 @@
 import { createApp } from "vue";
 import App from "./App.vue";
-// import { registerSW } from "virtual:pwa-register";
+import { registerSW } from "virtual:pwa-register";
 import "./index.css";
 
-// @todo: activate when deploying
-// if ("serviceWorker" in navigator) {
-//   registerSW();
-// }
+if ("serviceWorker" in navigator) {
+  registerSW();
+}
 
 createApp(App).mount("#application");
