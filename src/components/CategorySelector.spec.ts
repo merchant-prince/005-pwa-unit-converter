e12734625d90df35313fab0ffaed5c509c366405
Harivansh <hello@harivan.sh>
2022-04-27 12:00:00 +0400

    remove unnecessary tests

--- src/components/CategorySelector.spec.ts
+++ src/components/CategorySelector.spec.ts
@@ -1,9 +1,9 @@
-import { describe, expect, it, test } from "vitest";
+import { describe, expect, it } from "vitest";
 import { mount } from "@vue/test-utils";
 import CategorySelector from "./CategorySelector.vue";
 
 describe("CategorySelector", () => {
-  it("can be rendered", () => {
+  it("renders correctly", () => {
     const name = "length";
     const wrapper = mount(CategorySelector, {
       props: {
@@ -29,34 +29,6 @@ describe("CategorySelector", () => {
     expect(wrapper.emitted()).toHaveProperty("update:modelValue");
     expect((wrapper.emitted("update:modelValue")![0] as string[])[0]).toBe(
       name
-    );
-  });
-
-  test("initial css classes", () => {
-    const name = "weight";
-    const wrapper = mount(CategorySelector, {
-      props: {
-        name,
-        modelValue: "",
-      },
-    });
-
-    ["text-gray-800", "border-gray-800"].forEach((cssClass) =>
-      expect(wrapper.classes()).toContain(cssClass)
-    );
-  });
-
-  test("selected css classes", async () => {
-    const name = "temperature";
-    const wrapper = mount(CategorySelector, {
-      props: {
-        name,
-        modelValue: name,
-      },
-    });
-
-    ["text-white", "border-orange-400", "bg-orange-400", "shadow-md"].forEach(
-      (cssClass) => expect(wrapper.classes()).toContain(cssClass)
     );
   });
 });
