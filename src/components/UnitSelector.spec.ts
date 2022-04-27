41542e7af63d668aa10dafb139b48aec98957b2f
Harivansh <hello@harivan.sh>
2022-04-27 15:25:46 +0400

    fix tests

--- src/components/UnitSelector.spec.ts
+++ src/components/UnitSelector.spec.ts
@@ -1,36 +1,37 @@
 import { describe, expect, it } from "vitest";
 import { mount } from "@vue/test-utils";
 import UnitSelector from "./UnitSelector.vue";
+import { units } from "../lib/units";
 
 describe("UnitSelector", () => {
   it("renders correctly", () => {
-    const units = ["millimeter", "centimeter", "meter", "kilometer"];
     const wrapper = mount(UnitSelector, {
       props: {
-        units,
-        modelValue: "",
+        units: units.length,
+        modelValue: units.length[0],
       },
     });
 
-    units.forEach((unit) => {
-      expect(wrapper.text()).toContain(unit);
+    units.length.forEach((unit) => {
+      expect(wrapper.text()).toContain(unit.name);
     });
   });
 
   it("emits 'update:modelValue' with the clicked 'unit' - when clicked", async () => {
-    const units = ["millimeter", "centimeter", "meter", "kilometer"];
     const wrapper = mount(UnitSelector, {
       props: {
-        units,
-        modelValue: "",
+        units: units.temperature,
+        modelValue: units.temperature[0],
       },
     });
 
-    await wrapper.find(`[data-test="unit-${units[1]}"]`).trigger("click");
+    await wrapper
+      .find(`[data-test="unit-${units.temperature[1].name}"]`)
+      .trigger("click");
 
     expect(wrapper.emitted()).toHaveProperty("update:modelValue");
-    expect((wrapper.emitted("update:modelValue")![0] as string[])[0]).toBe(
-      units[1]
-    );
+    expect(
+      (wrapper.emitted("update:modelValue")![0] as string[])[0]
+    ).toMatchObject(units.temperature[1]);
   });
 });
